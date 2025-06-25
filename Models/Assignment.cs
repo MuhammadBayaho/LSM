8e8bab2068658159d7ee0d767a39be12d0a081d3
MBMB <128155910+MuhammadBayaho@users.noreply.github.com>
2025-06-25 21:08:12 +0300

    Fix merge conflict markers

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-public class Assignment
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = "";
-    public string Description { get; set; } = "";
-    public DateTime DueDate { get; set; }
-
-    public int CourseId { get; set; }
-    public Course Course { get; set; }
-}
-=======
 namespace LMSApp.Models
 {
     public class Assignment
@@ -23,4 +11,3 @@ namespace LMSApp.Models
         public Course Course { get; set; }
     }
 }
->>>>>>> dbe7ede0baee7e9b1efa70f6d723820a0bc80ad7
--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-public class User
-{
-    public int Id { get; set; }
-    public string FullName { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string PasswordHash { get; set; } = "";
-    public string Role { get; set; } = "Student"; // or "Instructor"
-
-    public ICollection<Course> Courses { get; set; } = new List<Course>();
-    public ICollection<StudyGroup> StudyGroups { get; set; } = new List<StudyGroup>();
-}  
-=======
 namespace LMSApp.Models
 {
     public class User
@@ -25,4 +12,3 @@ namespace LMSApp.Models
         public ICollection<StudyGroup> StudyGroups { get; set; } = new List<StudyGroup>();
     }
 }
->>>>>>> dbe7ede0baee7e9b1efa70f6d723820a0bc80ad7
